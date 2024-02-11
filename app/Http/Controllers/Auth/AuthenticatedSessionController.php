63ae976322ca191ea234d06fdf8fa841d3bc5920
brunotrinchao <brunotrinchao@gmail.com>
2024-02-11 11:06:53 -0300

    [HEY-2] wip

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -14,7 +14,8 @@ class AuthenticatedSessionController extends Controller
     /**
      * Display the login view.
      */
-    public function create(): View{
+    public function create(): View
+    {
         return view('auth.login');
     }
 
