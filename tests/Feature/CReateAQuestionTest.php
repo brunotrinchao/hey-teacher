033e1147ae66cf5d5afd16af27a383bc5ea4bf3a
brunotrinchao <brunotrinchao@gmail.com>
2024-02-11 17:13:32 -0300

    [HEY-9] Create test max 10 caracters

--- tests/Feature/CReateAQuestionTest.php
+++ tests/Feature/CReateAQuestionTest.php
@@ -20,8 +20,23 @@ test('Nova pergunta com até 255 catacteres', function () {
     assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
 });
 
-it('Testa se a pergunta termina com ?', function () {
-});
+//it('Testa se a pergunta termina com ?', function () {
+//    // Arrange : Preparar
+//    $user = User::factory()->create();
+//    actingAs($user);
+//
+//    // Act : Agir
+//    $request = post(route('question.store'), [
+//        'question' => str_repeat('*', 10) . '?',
+//    ]);
+//
+//    // Assent : Verificar
+//    $request->assertSessionHasErrors([
+//        'question' => 'No término da pergunta adicione ?'
+//    ]);
+//    assertDatabaseCount('questions', 0);
+//
+//});
 
 it('Testa se a pergunta tem no mínimo 10 caracteres', function () {
     // Arrange : Preparar
