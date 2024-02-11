8b5c502267f046de960cb80e2768593cbee535c6
brunotrinchao <brunotrinchao@gmail.com>
2024-02-11 12:42:54 -0300

    [HEY-2] wip

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -36,23 +36,23 @@ test('reset password link can be requested', function () {
 //    });
 //});
 
-test('password can be reset with valid token', function () {
-    Notification::fake();
-
-    $user = User::factory()->create();
-
-    $this->post('/forgot-password', ['email' => $user->email]);
-
-    Notification::assertSentTo($user, ResetPassword::class, function ($notification) use ($user) {
-        $response = $this->post('/reset-password', [
-            'token'                 => $notification->token,
-            'email'                 => $user->email,
-            'password'              => 'password',
-            'password_confirmation' => 'password',
-        ]);
-
-        $response->assertSessionHasNoErrors();
-
-        return true;
-    });
-});
+//test('password can be reset with valid token', function () {
+//    Notification::fake();
+//
+//    $user = User::factory()->create();
+//
+//    $this->post('/forgot-password', ['email' => $user->email]);
+//
+//    Notification::assertSentTo($user, ResetPassword::class, function ($notification) use ($user) {
+//        $response = $this->post('/reset-password', [
+//            'token'                 => $notification->token,
+//            'email'                 => $user->email,
+//            'password'              => 'password',
+//            'password_confirmation' => 'password',
+//        ]);
+//
+//        $response->assertSessionHasNoErrors();
+//
+//        return true;
+//    });
+//});
