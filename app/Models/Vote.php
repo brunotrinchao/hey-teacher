71b954a0ebcdf984316adb47c42f2c8f3218d1e0
brunotrinchao <brunotrinchao@gmail.com>
2024-02-13 00:24:40 -0300

    [HEY-12] Fix N+1 sql

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vote extends Model
 {
     use HasFactory;
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -7,11 +7,12 @@
     <span>{{ $question->question  }}</span>
 
     <div>
+
         <x-form :action="route('question.like', $question)">
 
             <button class="flex items-start space-x-2 text-green-400">
                 <x-icon.thumbs-up class="w-5 h-5 text-green-500 hover:text-green-300 cursor-pointer"/>
-                <span>{{ $question->likes }}</span>
+                <span>{{ $question->votes_sum_like ?: 0 }}</span>
             </button>
 
         </x-form>
@@ -20,7 +21,7 @@
 
             <button class="flex items-start space-x-2 text-green-400">
                 <x-icon.thumbs-down class="w-5 h-5 text-red-500 hover:text-red-300 cursor-pointer"/>
-                <span>{{ $question->unlikes }}</span>
+                <span>{{ $question->votes_sum_unlike ?: 0 }}</span>
             </button>
 
         </x-form>
