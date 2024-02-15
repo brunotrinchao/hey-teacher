0f9f3fcf1530e6b39d6646df0f54ba48fde4557d
brunotrinchao <brunotrinchao@gmail.com>
2024-02-14 21:45:35 -0300

    [HEY-17] Editando pergunta

--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -12,7 +12,7 @@ test('Atualiza pergunta no banco', function () {
 
     put(route('question.update', $question), [
         'question' => 'Updated Question?',
-    ])->assertRedirect('question.index');
+    ])->assertRedirect(route('question.index'));
 
     $question->refresh();
 
