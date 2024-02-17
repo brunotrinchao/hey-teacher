9168dfabb4cd635bb6bfd7e0132429d4703832c3
brunotrinchao <brunotrinchao@gmail.com>
2024-02-17 13:18:34 -0300

    [HEY-23] Deletando automaticamente 1 mes

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,15 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
-use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Illuminate\Database\Eloquent\{Model, Prunable, SoftDeletes};
 
 class Question extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Prunable;
 
     public mixed $draft;
 
@@ -34,8 +36,15 @@ class Question extends Model
     protected function likes(): Attribute
     {
         return new Attribute(get: fn () => $this->votes()->sum('like'));
-    }    protected function unlikes(): Attribute
+    }
+
+    protected function unlikes(): Attribute
     {
         return new Attribute(get: fn () => $this->votes()->sum('unlike'));
+    }
+
+    public function prunable(): Builder
+    {
+        return static::where('deleted_at', '<=', now()->subMonths());
     }
 }
