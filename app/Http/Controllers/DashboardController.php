82fc9f181e421893d7c64475c7373fad651a7b10
brunotrinchao <brunotrinchao@gmail.com>
2024-02-17 12:16:58 -0300

    [HEI-19] Test order link and unlike

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,6 +13,7 @@ class DashboardController extends Controller
             'questions' => Question::query()
                 ->withSum('votes', 'like')
                 ->withSum('votes', 'unlike')
+                ->orderByRaw('CASE WHEN votes_sum_like IS NULL THEN 0 ELSE votes_sum_like END DESC, CASE WHEN votes_sum_unlike IS NULL THEN 0 ELSE votes_sum_unlike END')
                 ->paginate(5),
         ]);
     }
--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -3,7 +3,7 @@
 use App\Models\{Question, User};
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
-use function Pest\Laravel\{actingAs, get};
+use function Pest\Laravel\{actingAs, get, withoutExceptionHandling};
 
 test('Lista all the Questions', function () {
     // Arrange : Preparar
@@ -32,6 +32,33 @@ test('Paginate result', function () {
     get(route('dashboard'))
         ->assertViewHas('questions', function ($value) {
             return $value instanceof LengthAwarePaginator;
+        });
+
+    // Assent : Verificar
+});
+
+test('List link and unlike order', function () {
+    // Arrange : Preparar
+    $user       = User::factory()->create();
+    $secundUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+    $mostLikeQuestion   = Question::find(3);
+    $mostUnlikeQuestion = Question::find(1);
+    $user->like($mostLikeQuestion);
+    $secundUser->unlike($mostUnlikeQuestion);
+    actingAs($user);
+
+    withoutExceptionHandling();
+
+    // Act : Agir
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) use ($mostLikeQuestion, $mostUnlikeQuestion) {
+            expect($questions)
+                ->first()->id->toBe(3)
+                ->and($questions)
+                ->last()->id->toBe(1);
+
+            return true;
         });
 
     // Assent : Verificar
